16ab197fbf3cf99925bce8bc1d12f8fa6ff61513
JeremiahTrinh <demiahtrinh725@gmail.com>
2025-11-07 11:00:12 -0600

    Updated FileProblems/dictionary.json and completed fileAssignment4.py

--- Week1Project/CodingAssignments/FileProblems/fileAssignment4.py
+++ Week1Project/CodingAssignments/FileProblems/fileAssignment4.py
@@ -6,6 +6,25 @@ import sys
 
 # Read data from file and allow for searching data, returning relevant data.
 # Start with name search then can go to any data search.
+
+def search_data(list_of_dicts, key_name, target):
+    target_list = []
+    for dictionary_e in list_of_dicts:
+        if target in dictionary_e[key_name]:
+            target_list.append(dictionary_e)
+    return target_list
+
+def search_data_for_students(list_of_dicts, target):
+    target_list = []
+    for dictionary_e in list_of_dicts:
+        if target == dictionary_e["isStudent"]:
+            target_list.append(dictionary_e)
+    return target_list
+
+def print_results(results):
+    print(f"Found {len(results)} result{"s" if len(results) > 1 else ""}.")
+    print(results)
+
 json_file_name = "dictionary.json"
 try:
     with open(json_file_name, 'r') as file:
@@ -21,15 +40,31 @@ except json.JSONDecodeError:
 else:
     match input("Enter search criteria: ").lower():
         case "name":
-            target = input("Enter target name: ").capitalize()
-            print(f"{data["name"]}")
+            target_name = input("Enter target name: ").capitalize()
+            print_results(search_data(data, "name", target_name))
         case "age":
-            pass
+            target_age = int(input("Enter target age: "))
+            print_results(search_data(data, "age", target_age))
         case "city":
-            pass
+            target_city = input("Enter target city: ")
+            print_results(search_data(data, "city", target_city))
         case "isstudent" | "student":
-            pass
+            isStudent_map = {
+                "true": True,
+                "false": False,
+                "t": True,
+                "f": False,
+                "yes": True,
+                "no": False,
+                "y": True,
+                "n": False,
+                "1": True,
+                "0": False
+            }
+            target_isStudent = isStudent_map.get(input(f"Are you searching for students (y/n)? ").lower())
+            print_results(search_data_for_students(data, target_isStudent))
         case "course" | "courses":
-            pass
+            target_course = input("Enter target course: ").capitalize()
+            print_results(search_data(data, "courses", target_course))
         case _:
             print("Error: Invalid search criteria.")

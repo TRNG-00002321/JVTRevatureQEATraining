66deed9cf0bbb29ce698446fbda6fa3a7a191f4a
JeremiahTrinh <demiahtrinh725@gmail.com>
2025-11-17 15:27:17 -0600

    Added Manager side of project to P0

---
+++ P0/Manager/src/main/java/com/revature/manager/ManagerDBManager.java
@@ -0,0 +1,13 @@
+package com.revature.manager;
+
+/*
+TODO
+[] Log in securely so that I can access and manage employee expense reports.
+[] View a list of all pending expenses so that I can review them efficiently.
+[] Approve or deny submitted expenses so that I can manage reimbursements appropriately.
+[] Add comments to expense decisions so that employees understand the reasoning behind approvals or denials.
+[] Generate reports by employee, category, or date so that I can analyze spending trends and make informed decisions.
+ */
+
+public class ManagerDBManager {
+}

9c748ed40d35dbb46875460216cd5b22e5b6d150
JeremiahTrinh <demiahtrinh725@gmail.com>
2025-11-17 15:59:28 -0600

    Added User.java to Manager side of P0

---
+++ P0/Manager/src/main/java/com/revature/manager/User.java
@@ -0,0 +1,52 @@
+package com.revature.manager;
+
+import java.util.Objects;
+
+public abstract class User {
+    private int id;
+    private String name;
+    private String password;
+    private String role;
+
+    public User(int id, String name, String password, String role){
+        this.id = id;
+        this.name = name;
+        this.password = password;
+        this.role = role;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public abstract boolean isEmployee();
+    public abstract boolean isManager();
+}

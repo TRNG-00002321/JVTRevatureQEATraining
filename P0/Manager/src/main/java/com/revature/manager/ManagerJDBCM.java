8ccb47f62380e7da7f961c3524efeace684e6d97
JeremiahTrinh <demiahtrinh725@gmail.com>
2025-11-18 14:53:52 -0600

    Updated ManagerJDBCM.java

--- P0/Manager/src/main/java/com/revature/manager/ManagerJDBCM.java
+++ P0/Manager/src/main/java/com/revature/manager/ManagerJDBCM.java
@@ -11,9 +11,9 @@ import java.util.ArrayList;
 TODO
 [X] Log in securely so that I can access and manage employee expense reports.
 [X] View a list of all pending expenses so that I can review them efficiently.
-[] Approve or deny submitted expenses so that I can manage reimbursements appropriately.
-[] Add comments to expense decisions so that employees understand the reasoning behind approvals or denials.
-[] Generate reports by employee, category, or date so that I can analyze spending trends and make informed decisions.
+[X] Approve or deny submitted expenses so that I can manage reimbursements appropriately.
+[X] Add comments to expense decisions so that employees understand the reasoning behind approvals or denials.
+[X] Generate reports by employee, category, or date so that I can analyze spending trends and make informed decisions.
  */
 
 public class ManagerJDBCM {
@@ -98,13 +98,33 @@ public class ManagerJDBCM {
         return false;
     }
 
+    public List<Expense> getAllExpenses() {
+        String sql = "SELECT e.id, e.user_id, e.amount, e.description, e.date FROM expenses AS e JOIN approvals AS a ON e.id = a.expense_id;";
+        List<Expense> expenses = new ArrayList<>();
+
+        try (PreparedStatement pstmt = connection.prepareStatement(sql)) {
+            ResultSet rs = pstmt.executeQuery();
+            while (rs.next()) {
+                int expenseId = rs.getInt("id");
+                int userId = rs.getInt("user_id");
+                double amount = rs.getDouble("amount");
+                String desc = rs.getString("description");
+                String dateString = rs.getString("date");
+                LocalDate date = LocalDate.parse(dateString);
+
+                expenses.add(new Expense(expenseId, userId, amount, desc, date));
+            }
+        } catch (SQLException e) {
+            System.out.println("Error querying SQLite database connection: " + e.getMessage());
+        }
+
+        return expenses;
+    }
+
     public List<Expense> getPendingExpenses() {
         String sql = "SELECT e.id, e.user_id, e.amount, e.description, e.date FROM expenses AS e JOIN approvals AS a ON e.id = a.expense_id WHERE a.status = ?;";
         List<Expense> pendingExpenses = new ArrayList<>();
-        // SELECT e.id, e.user_id, e.amount, e.description, e.date
-        //        FROM expenses AS e
-        //        JOIN approvals AS a ON e.id = a.expense_id
-        //        WHERE a.status = ?;
+
         try (PreparedStatement pstmt = connection.prepareStatement(sql)) {
             pstmt.setString(1, "pending");
             ResultSet rs = pstmt.executeQuery();
@@ -125,11 +145,39 @@ public class ManagerJDBCM {
         return pendingExpenses;
     }
 
-    public void approveExpense(int expenseId, int reviewerId, String comment, LocalDate reviewDate) {
-        //
+    public int approveExpense(int approvalId, int reviewerId, String comment) {
+        LocalDate reviewDate = LocalDate.now();
+        String sql = "UPDATE approvals SET status = ?, reviewer = ?, comment = ?, review_date = ? WHERE id = ?";
+
+        try (PreparedStatement pstmt = connection.prepareStatement(sql)) {
+            pstmt.setString(1, "approved");
+            pstmt.setInt(2, reviewerId);
+            pstmt.setString(3, comment);
+            pstmt.setString(4, reviewDate.toString());
+            pstmt.setInt(5, approvalId);
+            return pstmt.executeUpdate();
+        } catch (SQLException e) {
+            System.out.println("Error querying SQLite database connection: " + e.getMessage());
+        }
+
+        return -1;
     }
 
-    public void denyExpense(int expenseId, int reviewerId, String comment, LocalDate reviewDate) {
-        //
+    public int denyExpense(int approvalId, int reviewerId, String comment) {
+        LocalDate reviewDate = LocalDate.now();
+        String sql = "UPDATE approvals SET status = ?, reviewer = ?, comment = ?, review_date = ? WHERE id = ?";
+
+        try (PreparedStatement pstmt = connection.prepareStatement(sql)) {
+            pstmt.setString(1, "denied");
+            pstmt.setInt(2, reviewerId);
+            pstmt.setString(3, comment);
+            pstmt.setString(4, reviewDate.toString());
+            pstmt.setInt(5, approvalId);
+            return pstmt.executeUpdate();
+        } catch (SQLException e) {
+            System.out.println("Error querying SQLite database connection: " + e.getMessage());
+        }
+
+        return -1;
     }
 }

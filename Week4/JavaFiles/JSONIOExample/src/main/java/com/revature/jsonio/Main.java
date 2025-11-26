f4bacdce5982b12ddc93a52aab2fb2d93c39b4e2
JeremiahTrinh <demiahtrinh725@gmail.com>
2025-11-26 10:53:56 -0600

    Modified Main.java for JSONIOExample and added documentation for P0

--- Week4/JavaFiles/JSONIOExample/src/main/java/com/revature/jsonio/Main.java
+++ Week4/JavaFiles/JSONIOExample/src/main/java/com/revature/jsonio/Main.java
@@ -11,11 +11,13 @@ public class Main {
     public static void main(String[] args) {
         File employeeJson = new File("employees.json");
         File studentJson = new File("students.json");
+
         Employee emp1 = new Employee(101, "Jerry Dingdoo", "1234 Street Drive", "Worker");
         Employee emp2 = new Employee(104, "Jadie Vance", "2478 Wow Blvd", "Programmer");
         Employee emp3 = new Employee(107, "Paul Salto", "9878 Wilson Street", "Constructor");
         Employee emp4 = new Employee(231, "Zeffery Williams", "1378 Stool Dr", "Imploder");
         Employee emp5 = new Employee(963, "Lingar Pingar", "3674 Mustard Way", "Licker");
+
         Student s1 = new Student(251844, "Larry Sue", 3.673);
         Student s2 = new Student(987322, "Mary Doog", 4.0);
         Student s3 = new Student(513667, "Kendrick Pamar", 3.367);
@@ -36,5 +38,6 @@ public class Main {
         Optional<Employee> test = empService.get(101);
         System.out.println(test.orElse(new Employee(0, "Michael McDoesn't-Exist", "XXXX Not Real Street", "None")));
 
+        //
     }
 }

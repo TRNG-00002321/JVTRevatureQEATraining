8f92d6293332f42866a627e533e57113f319cdcb
JeremiahTrinh <89665166+JeremiahTrinh@users.noreply.github.com>
2025-12-24 12:42:28 -0600

    Added CucumberBDD

---
+++ Week8/CucumberBDD/src/test/java/com/revature/cuc/steps/LoginSteps.java
@@ -0,0 +1,79 @@
+package com.revature.cuc.steps;
+
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.junit.jupiter.api.Assertions;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class LoginSteps {
+    private WebDriver driver;
+    private final String BASE_URL = "https://the-internet.herokuapp.com/login";
+
+    @Before
+    public void setUp() {
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+    }
+
+    @After
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+
+    @Given("the application is running")
+    public void theApplicationIsRunning() {
+        // Write code here that turns the phrase above into concrete actions
+        driver.get(BASE_URL);
+        Assertions.assertTrue(driver.getTitle().contains("The Internet"));
+    }
+
+    @And("the test database is already seeded with users")
+    public void theTestDatabaseIsAlreadySeededWithUsers() {
+        // Write code here that turns the phrase above into concrete actions
+        System.out.println("the test database is already seeded with users");
+    }
+
+    @Given("the user is on the login page")
+    public void theUserIsOnTheLoginPage() {
+        // Write code here that turns the phrase above into concrete actions
+        driver.get(BASE_URL);
+    }
+
+    @When("the user enters username {string}")
+    public void theUserEntersUsername(String arg0) {
+        // Write code here that turns the phrase above into concrete actions
+        driver.findElement(By.id("username")).sendKeys(arg0);
+    }
+
+    @And("the user enters password {string}")
+    public void theUserEntersPassword(String arg0) {
+        // Write code here that turns the phrase above into concrete actions
+        driver.findElement(By.id("password")).sendKeys(arg0);
+    }
+
+    @And("the user clicks the login button")
+    public void theUserClicksTheLoginButton() {
+        // Write code here that turns the phrase above into concrete actions
+        driver.findElement(By.cssSelector(".fa.fa-2x.fa-sign-in")).click();
+    }
+
+    @Then("the user should be redirected to the secure area")
+    public void theUserShouldBeRedirectedToTheSecureArea() {
+        // Write code here that turns the phrase above into concrete actions
+        Assertions.assertTrue(driver.getCurrentUrl().contains("/secure"));
+    }
+
+    @And("the page should display a message containing {string}")
+    public void thePageShouldDisplayAMessageContaining(String arg0) {
+        // Write code here that turns the phrase above into concrete actions
+        Assertions.assertTrue(driver.findElement(By.id("flash")).getText().contains(arg0));
+    }
+}
